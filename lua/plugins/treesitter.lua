769e1b87cced488c568892126d1d2a588bd7b03a
Stephen Tudor <smt@smt.io>
2024-04-09 09:18:58 -0400

    Ensure tree-sitter parsers are installed for other filetypes I use

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -6,14 +6,24 @@ return {
   opts = function(_, opts)
     -- add more things to the ensure_installed table protecting against community packs modifying it
     opts.ensure_installed = require("astrocore").list_insert_unique(opts.ensure_installed, {
+      "bash",
+      "css",
+      "dockerfile",
       "eex",
       "elixir",
       "graphql",
       "heex",
+      "html",
+      "javascript",
+      "jsdoc",
+      "json",
       "ledger",
       "lua",
+      "python",
       "sql",
       "toml",
+      "tmux",
+      "typescript",
       "vim",
       "yaml",
       -- add more arguments for adding more treesitter parsers
